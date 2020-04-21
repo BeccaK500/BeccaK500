16eb9069f73760f9ccea8b249e0564f1a8ed48ca
Becca Keenan <rk@keeresources.com>
2020-04-21 09:03:28 +0100

    End of Section 7

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -10,6 +10,12 @@ import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptor, ErrorInterceptorProvider } from './_services/error.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { RouterModule } from '@angular/router';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { appRoutes } from './routes';
+
 
 
 @NgModule({
@@ -17,19 +23,24 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
       BrowserAnimationsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
+      BsDropdownModule.forRoot(),
+      RouterModule.forRoot(appRoutes),
+
    ],
    providers: [
       AuthService,
       ErrorInterceptorProvider
-
    ],
    bootstrap: [
       AppComponent
